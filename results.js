3c9c2496fd3534f2b117271ac1cc2664573d9606
Wrenen D'Cunha <105389231+Wrenen@users.noreply.github.com>
2023-03-26 19:44:06 -0400

    Create results.js

---
+++ results.js
@@ -0,0 +1,45 @@
+console.log('JavaScript imported successfully');
+
+function displayCreate(){
+    let createElement= document.getElementById('create');
+
+    createElement.style.display = 'block';
+}
+function hideCreate(){
+    let createElement= document.getElementById('create');
+
+    createElement.style.display = 'none';
+}
+function displayPopulate(){
+    let populateElement= document.getElementById('populate');
+
+    populateElement.style.display = 'block';
+}
+function hidePopulate(){
+    let populateElement= document.getElementById('populate');
+
+    populateElement.style.display = 'none';
+}
+
+function displayQuery (){
+    let queryElement= document.getElementById('query');
+
+    queryElement.style.display = 'block';
+}
+
+function hideQuery (){
+    let queryElement = document.getElementById('query');
+
+    queryElement.style.display = 'none';
+}
+
+function displayDrop(){
+    let dropElement= document.getElementById('drop');
+
+    dropElement.style.display = 'block';
+}
+function hideDrop(){
+    let dropElement= document.getElementById('drop');
+
+    dropElement.style.display = 'none';
+}
